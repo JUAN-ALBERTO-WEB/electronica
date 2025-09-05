6b48c199fdc0f18456d0358cb4fc7aa0a79e2621
JUAN-ALBERTO-WEB <alexepiet@gmail.com>
2025-09-05 14:30:56 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CongresoElectronicaLanding from './CongresoElectronicaLanding'
+
+function App() {
+  return <CongresoElectronicaLanding />
+}
+
+export default App
